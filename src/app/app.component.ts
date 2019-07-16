7045417f7bb5637a3b97b26e2e5c9a47537ec3eb
Maina Wycliffe <wmmaina7@gmail.com>
2019-07-16 23:29:24 +0300

    add ui for the demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-async-validator';
+export class AppComponent implements OnInit {
+  frmLogin: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.frmLogin = this.createForm();
+  }
+
+  ngOnInit() {}
+
+  hasError(field: string, error: string): boolean {
+    return false;
+  }
+
+  createForm(): FormGroup {
+    return this.fb.group({
+      username: [null, [Validators.required]],
+      password: [null, [Validators.required]]
+    });
+  }
 }
